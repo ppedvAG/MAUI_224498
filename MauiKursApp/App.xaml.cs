4d12978f9c0dd276c59fe0495706bf1c0c30ee94
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-02-06 11:29:39 +0100

    Add project files.

---
+++ MauiKursApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace MauiKursApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        //MainPage beinhaltet die jeweils angezeigte Seite. Zuweisungen hier definieren die Startpage der App.
+        MainPage = new AppShell();
+	}
+
+
+    //Override der CreateWindow() um Zugriff auf globale shared Lifecycle-Events zu haben (für OS-spezifische LC-Events siehe MauiProgram.cs)
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Created += (s, e) => Debug.Print("SharedCreated");
+        window.Stopped += (s, e) => Debug.Print("SharedStopped");
+
+        return window;
+    }
+}
---
+++ MauiKursApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiKursApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
